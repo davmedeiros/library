f96f73d50a187e7f5289be25664800c454240157
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-03 17:11:56 +0100

    Create Book constructor and addBookToLibrary()

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let library = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    library.push(new Book(title, author, pages, isRead));
+}
