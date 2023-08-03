88b84f7fb6492704f07307239d187b5f9c39b163
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-03 17:22:37 +0100

    Add displayLibrary()
    This function displays all books in the library

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(title, author, pages, isRead) {
     library.push(new Book(title, author, pages, isRead));
 }
+
+function displayLibrary() {
+    library.forEach(book => console.log(book));
+}
+
+// TEST
+addBookToLibrary('Open Window', 'T.J. Roosevelt', 341, true);
+addBookToLibrary('Mysteries Of The Hallow Grove', 'Jane Garner', 295, false);
+addBookToLibrary('Cloud Landing', 'Hui Jun', 164, false);
+
+displayLibrary();
