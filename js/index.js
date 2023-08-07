532428cc9d0e828bed2b4bda4c3853d3353ab705
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-07 13:02:19 +0100

    Display books on page

--- js/index.js
+++ js/index.js
@@ -12,10 +12,30 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 function displayLibrary() {
-    library.forEach(book => console.log(book));
+    const shelf = document.querySelector('main');
+    library.forEach(book => {
+        const cover = document.createElement('div');
+        cover.classList.add('book');
+        const title = document.createElement('h3');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const status = document.createElement('p');
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        status.textContent = book.isRead ? 'read' : 'not read';
+        cover.appendChild(title);
+        cover.appendChild(author);
+        cover.appendChild(pages);
+        cover.appendChild(status);
+        shelf.appendChild(cover);
+    });
 }
+
 
 // TEST
 addBookToLibrary('Open Window', 'T.J. Roosevelt', 341, true);
 addBookToLibrary('Mysteries Of The Hallow Grove', 'Jane Garner', 295, false);
 addBookToLibrary('Cloud Landing', 'Hui Jun', 164, false);
+
+displayLibrary();
