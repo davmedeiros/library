cd8af25c2349521c01578cde53f6c27e3164b8ba
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-15 10:59:53 +0100

    Run prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # library
+
 TOP - Library
--- js/index.js
+++ js/index.js
@@ -1,132 +1,131 @@
 let library = [];
 
 function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-    library.push(new Book(title, author, pages, isRead));
+  library.push(new Book(title, author, pages, isRead));
 }
 
 function clearShelf(shelf) {
-    while (shelf.firstChild) {
-        shelf.removeChild(shelf.firstChild);
-    }
+  while (shelf.firstChild) {
+    shelf.removeChild(shelf.firstChild);
+  }
 }
 
 function displayLibrary() {
-    const shelf = document.querySelector('main');
-    clearShelf(shelf);
-    library.forEach((book, index) => {
-        const cover = document.createElement('div');
-        cover.classList.add('book');
-        const title = document.createElement('h3');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const status = document.createElement('p');
-        const actions = document.createElement('div');
-        actions.classList.add('actions');
-        const remove = document.createElement('button');
-        remove.textContent = '\u2717';       
-        actions.appendChild(remove);
-        const toggleRead = document.createElement('button');
-        toggleRead.textContent = '\u2713';
-        actions.appendChild(toggleRead);
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        status.textContent = book.isRead ? 'read' : 'not read';
-        cover.dataset.indexNumber = index;
-        cover.appendChild(title);
-        cover.appendChild(author);
-        cover.appendChild(pages);
-        cover.appendChild(status);
-        cover.appendChild(actions);
-        shelf.appendChild(cover);
+  const shelf = document.querySelector('main');
+  clearShelf(shelf);
+  library.forEach((book, index) => {
+    const cover = document.createElement('div');
+    cover.classList.add('book');
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const status = document.createElement('p');
+    const actions = document.createElement('div');
+    actions.classList.add('actions');
+    const remove = document.createElement('button');
+    remove.textContent = '\u2717';
+    actions.appendChild(remove);
+    const toggleRead = document.createElement('button');
+    toggleRead.textContent = '\u2713';
+    actions.appendChild(toggleRead);
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    status.textContent = book.isRead ? 'read' : 'not read';
+    cover.dataset.indexNumber = index;
+    cover.appendChild(title);
+    cover.appendChild(author);
+    cover.appendChild(pages);
+    cover.appendChild(status);
+    cover.appendChild(actions);
+    shelf.appendChild(cover);
 
-        remove.addEventListener('click', () => {
-            library.splice(cover.dataset.indexNumber, 1);
-            clearShelf(shelf);
-            displayLibrary();
-        })
-
-        toggleRead.addEventListener('click', () => {
-            library[cover.dataset.indexNumber].isRead = !library[cover.dataset.indexNumber].isRead;
-            clearShelf(shelf);
-            displayLibrary();
-        })
+    remove.addEventListener('click', () => {
+      library.splice(cover.dataset.indexNumber, 1);
+      clearShelf(shelf);
+      displayLibrary();
     });
+
+    toggleRead.addEventListener('click', () => {
+      library[cover.dataset.indexNumber].isRead =
+        !library[cover.dataset.indexNumber].isRead;
+      clearShelf(shelf);
+      displayLibrary();
+    });
+  });
 }
 
 (function ControlCenter() {
-    const aside = document.querySelector('aside');
-    const newBookButton = document.querySelector('#new-book');
+  const aside = document.querySelector('aside');
+  const newBookButton = document.querySelector('#new-book');
 
-    this.clear = () => {
-        while (aside.firstChild) {
-            aside.removeChild(aside.firstChild);
-        }
+  this.clear = () => {
+    while (aside.firstChild) {
+      aside.removeChild(aside.firstChild);
     }
+  };
 
-    this.addNewBookBehavior = (button) => {
-        button.addEventListener('click', () => {
-            this.clear();
-            this.displayNewBookForm();
-        });
-    }
+  this.addNewBookBehavior = (button) => {
+    button.addEventListener('click', () => {
+      this.clear();
+      this.displayNewBookForm();
+    });
+  };
 
-    this.addNewBookBehavior(newBookButton);
+  this.addNewBookBehavior(newBookButton);
 
-    this.displayNewBookForm = () => {
-        const form = document.createElement('form');
-        const title = document.createElement('input');
-        title.classList.add('title');
-        title.type = 'text';
-        title.placeholder = 'Title';
-        const author = document.createElement('input');
-        author.classList.add('author');
-        author.type = 'text';
-        author.placeholder = 'Author';
-        const pages = document.createElement('input');
-        pages.classList.add('pages');
-        pages.type = 'text';
-        pages.placeholder = 'Pages';
-        const labelStatus = document.createElement('label');
-        labelStatus.setAttribute('for', 'status');
-        labelStatus.textContent = 'Mark as read?';
-        const status = document.createElement('input');
-        status.id = 'read';
-        status.type = 'checkbox';
-        const submit = document.createElement('button');
-        submit.type = 'button';
-        submit.textContent = 'Add Book';
-        submit.id = 'submit-book';
-        form.appendChild(title);
-        form.appendChild(author);
-        form.appendChild(pages);
-        form.appendChild(labelStatus);
-        form.appendChild(status);
-        form.appendChild(submit);
+  this.displayNewBookForm = () => {
+    const form = document.createElement('form');
+    const title = document.createElement('input');
+    title.classList.add('title');
+    title.type = 'text';
+    title.placeholder = 'Title';
+    const author = document.createElement('input');
+    author.classList.add('author');
+    author.type = 'text';
+    author.placeholder = 'Author';
+    const pages = document.createElement('input');
+    pages.classList.add('pages');
+    pages.type = 'text';
+    pages.placeholder = 'Pages';
+    const labelStatus = document.createElement('label');
+    labelStatus.setAttribute('for', 'status');
+    labelStatus.textContent = 'Mark as read?';
+    const status = document.createElement('input');
+    status.id = 'read';
+    status.type = 'checkbox';
+    const submit = document.createElement('button');
+    submit.type = 'button';
+    submit.textContent = 'Add Book';
+    submit.id = 'submit-book';
+    form.appendChild(title);
+    form.appendChild(author);
+    form.appendChild(pages);
+    form.appendChild(labelStatus);
+    form.appendChild(status);
+    form.appendChild(submit);
 
-        submit.addEventListener('click', () => {
-            addBookToLibrary(title.value, author.value, pages.value, status.checked);
-            displayLibrary();
-            this.clear();
-            const newBook = document.createElement('button');
-            newBook.id = 'new-book';
-            newBook.textContent = '+';
-            this.addNewBookBehavior(newBook);
-            aside.appendChild(newBook);
-        })
+    submit.addEventListener('click', () => {
+      addBookToLibrary(title.value, author.value, pages.value, status.checked);
+      displayLibrary();
+      this.clear();
+      const newBook = document.createElement('button');
+      newBook.id = 'new-book';
+      newBook.textContent = '+';
+      this.addNewBookBehavior(newBook);
+      aside.appendChild(newBook);
+    });
 
-        aside.appendChild(form);
-    }
+    aside.appendChild(form);
+  };
 })();
-
-
 
 // TEST
 addBookToLibrary('Open Window', 'T.J. Roosevelt', 341, true);
